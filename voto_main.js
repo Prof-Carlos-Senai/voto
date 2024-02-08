34ca963296efd3441008b8e338a2b727796d0f20
Carlos <silva@edu.sc.senai.br>
2024-02-08 11:06:32 -0300

    adicionado a classe e os estilos

--- voto_main.js
+++ voto_main.js
@@ -6,5 +6,14 @@ verificar.addEventListener('click', ()=>{
     let idade = Number(document.getElementById('idade').value)
     let titulo = Number(document.getElementById('titulo').value)
 
-    console.log(nome,idade,titulo)
+    console.log("dados do formulario: ",nome,idade,titulo)
+
+    const cidadao = new Eleitor()
+
+    cidadao.nome = nome
+    cidadao.idade = idade
+    cidadao.titulo = titulo
+
+    console.log("instância do objeto: ",cidadao)
+
 })
